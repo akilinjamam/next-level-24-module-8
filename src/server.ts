f2d610688e647415eb78567e8ce51bb337196398
akilinjamam <akilinjamam@gmail.com>
2024-05-17 17:00:31 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './app'
+import mongoose from 'mongoose'
+import config from './app/config'
+
+const port = config.port
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+
+    app.listen(port, () => {
+      console.log(`app listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
